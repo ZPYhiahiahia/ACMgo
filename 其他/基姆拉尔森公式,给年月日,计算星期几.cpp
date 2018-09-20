6cb4188c108780f9301308d6e36ca210da757929
ZPYhiahiahia <970936604@qq.com>
2018-09-20 21:43:25 +0800

    求星期几的公式

---
+++ 其他/基姆拉尔森公式,给年月日,计算星期几.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 这个整理的时候只需要下边那一句就行了, 就是那个公式. 
+int main(){
+	W = (D + 2 * M + 3 * (M + 1) \ 5 + Y + Y \ 4 - Y \ 100 + Y \ 400) Mod 7
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
