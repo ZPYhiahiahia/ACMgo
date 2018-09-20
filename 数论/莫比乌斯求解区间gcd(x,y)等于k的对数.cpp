2cadefa3e07c5ef015d415b1ab0405528da821b4
ZPYhiahiahia <970936604@qq.com>
2018-09-20 19:15:50 +0800

    莫比乌斯反演

---
+++ 数论/莫比乌斯求解区间gcd(x,y)等于k的对数.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<stack>
+#define MAX(a,b) a>b?a:b
+#define MIN(a,b) a<b?a:b
+#define rep(i,n) for(int (i)=0;(i)<n;(i)++)
+#define read(n) scanf("%d",&n);
+#define readll(n) scanf("%lld",&n);
+#define readdb(n) scanf("%lf",&n);
+#define readch(n) scanf("%c",&n);
+#define readstr(n) scanf("%s",n);
+#define test printf("test\n");
+#define init(n) memset(n,0,sizeof(n));
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef pair<int , int> p_ii;
+
+int IT_MAX = 1 << 19;
+int MOD = 1000000007;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const db PI = acos(-1);
+const db ERR = 1e-10;
+const int MAX_N = 100005;
+bool cmp (int a , int b){
+	return a>b;
+}
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
