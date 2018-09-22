b0bd958fae7e1f54b8911ea953e6e73fa337239d
ZPYhiahiahia <970936604@qq.com>
2018-09-22 17:11:22 +0800

    kmp简易版

---
+++ 字符串/kmp简易版c++函数.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std; 
+/*
+ *  strstr函数
+ *  功能：在串中查找指定字符串的第一次出现
+ *  用法：char *strstr(char *strOne, char *strTwo);
+ *  据说strstr函数和KMP的算法效率差不多
+ */
+
+int main(int argc, const char * argv[])
+{
+    char strOne[] = "Borland International";
+    char strTwo[] = "nation";
+    char *ptr;
+    ptr = strstr(strOne, strTwo);
+    std::cout << ptr << '\n';
+    return 0;
+}
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
